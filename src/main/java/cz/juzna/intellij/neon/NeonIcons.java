605e28a8c1619573d39420bee7fb3f872af1b069
Pavels.Veretennikovs <Pavels.Veretennikovs@4finance.com>
2015-05-20 17:43:36 +0300

    Release 0.5

--- src/main/java/cz/juzna/intellij/neon/NeonIcons.java
+++ src/main/java/cz/juzna/intellij/neon/NeonIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class NeonIcons {
-	public static Icon FILETYPE_ICON = IconLoader.getIcon("/icons/Neon.png", NeonIcons.class);
+	public static Icon FILETYPE_ICON = IconLoader.getIcon("/icons/Ansible.png", NeonIcons.class);
 }
