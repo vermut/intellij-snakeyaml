c3cf42dc32c29fdddc0426da9a3b8767ab80afab
Pavel Veretennikov <vermut@kid.lv>
2015-11-16 01:44:03 +0200

    error ignoring parser

--- src/main/java/lv/kid/vermut/intellij/yaml/lexer/ErrorFilterScanner.java
+++ src/main/java/lv/kid/vermut/intellij/yaml/lexer/ErrorFilterScanner.java
@@ -8,8 +8,19 @@ import java.io.Reader;
  * Created by VermutMac on 11/15/2015.
  */
 public class ErrorFilterScanner extends ErrorReportingScanner {
+
     public ErrorFilterScanner(Reader reader) {
         super(reader);
+    }
+
+    @Override
+    public boolean checkToken(Token.ID... choices) {
+/*
+        while (super.peekToken().getTokenId() == Token.ID.Error)
+            getToken();
+*/
+
+        return super.checkToken(choices);
     }
 
     @Override
