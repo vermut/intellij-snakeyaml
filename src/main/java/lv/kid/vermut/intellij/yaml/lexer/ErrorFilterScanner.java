a6dc742edd4b75e84e5e38d862a3384236cad9fc
Pavel Veretennikov <vermut@kid.lv>
2015-12-27 23:28:56 +0200

    whitespace scanner (no parser)

--- src/main/java/lv/kid/vermut/intellij/yaml/lexer/ErrorFilterScanner.java
+++ src/main/java/lv/kid/vermut/intellij/yaml/lexer/ErrorFilterScanner.java
@@ -3,18 +3,22 @@ package lv.kid.vermut.intellij.yaml.lexer;
 import org.yaml.snakeyaml.tokens.Token;
 
 import java.io.Reader;
+import java.util.EnumSet;
 
 /**
  * Created by VermutMac on 11/15/2015.
  */
 public class ErrorFilterScanner extends ErrorReportingScanner {
+
+    private EnumSet<Token.ID> filtered = EnumSet.of(Token.ID.Error, Token.ID.Whitespace, Token.ID.Comment);
+
     public ErrorFilterScanner(Reader reader) {
         super(reader);
     }
 
     @Override
     public Token peekToken() {
-        while (super.peekToken().getTokenId() == Token.ID.Error)
+        while (filtered.contains(super.peekToken().getTokenId()))
             getToken();
 
         return super.peekToken();
